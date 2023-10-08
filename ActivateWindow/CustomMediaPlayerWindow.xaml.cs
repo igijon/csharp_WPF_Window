c172f04577bccd76c71c8a9689d5fc264fa2fe5a
Inma Gijón <igijoninf@gmail.com>
2023-10-08 09:42:02 +0200

    Activar y desactivar ventana. Reproducir vídeo.

---
+++ ActivateWindow/CustomMediaPlayerWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ActivateWindow
+{
+    /// <summary>
+    /// Lógica de interacción para CustomMediaPlayerWindow.xaml
+    /// </summary>
+    public partial class CustomMediaPlayerWindow : Window
+    {
+        public CustomMediaPlayerWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show(Directory.GetCurrentDirectory());
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            mediaElement.Pause();
+        }
+    }
+}
---
+++ ActivateWindow/OtherWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ActivateWindow
+{
+    /// <summary>
+    /// Lógica de interacción para OtherWindow.xaml
+    /// </summary>
+    public partial class OtherWindow : Window
+    {
+        public OtherWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            //Estos métodos sirven para activar o desactivar contenidos si la ventana no está activa
+            Debug.WriteLine("Entré a ACTIVATED");
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Entré a DEACTIVATED");
+        }
+    }
+}
